7901daea9d68e00348828e4902ac12a0a2c2c722
SUBHAM ROY <subhamroy@SUBHAMs-MacBook-Air.local>
2024-12-09 23:54:21 +0530

    abcd

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,22 +5,22 @@ import { Stack } from '@mui/material';
 import Logo from '../assets/images/Logo.png';
 
 const Navbar = () => (
-  <Stack 
-    direction="row" 
-    justifyContent="space-around" 
-    sx={{ 
-      gap: { sm: '123px', xs: '40px' }, 
-      mt: { sm: '32px', xs: '20px' }, 
-      justifyContent: 'none' 
-    }} 
+  <Stack
+    direction="row"
+    justifyContent="space-around"
+    sx={{
+      gap: { sm: '123px', xs: '40px' },
+      mt: { sm: '32px', xs: '20px' },
+      justifyContent: 'none',
+    }}
     px="20px"
   >
     {/* Logo */}
     <Link to="/">
-      <img 
-        src={Logo} 
-        alt="logo" 
-        style={{ width: '48px', height: '48px', margin: '0px 20px' }} 
+      <img
+        src={Logo}
+        alt="logo"
+        style={{ width: '48px', height: '48px', margin: '0px 20px' }}
       />
     </Link>
 
@@ -36,34 +36,34 @@ const Navbar = () => (
       }}
     >
       {/* Home Link */}
-      <Link 
-        to="/" 
-        style={{ 
-          textDecoration: 'none', 
-          color: '#3A1212', 
-          borderBottom: '3px solid #FF2625' 
+      <Link
+        to="/"
+        style={{
+          textDecoration: 'none',
+          color: '#3A1212',
+          borderBottom: '3px solid #FF2625',
         }}
       >
         Home
       </Link>
 
       {/* Exercises Link */}
-      <a 
-        href="#exercises" 
-        style={{ 
-          textDecoration: 'none', 
-          color: '#3A1212' 
+      <a
+        href="#exercises"
+        style={{
+          textDecoration: 'none',
+          color: '#3A1212',
         }}
       >
         Exercises
       </a>
 
       {/* Fittrack Link */}
-      <a 
-        href="https://f-itness-track-master-seven.vercel.app/" 
-        style={{ 
-          textDecoration: 'none', 
-          color: '#3A1212' 
+      <a
+        href="https://f-itness-track-master-seven.vercel.app/"
+        style={{
+          textDecoration: 'none',
+          color: '#3A1212',
         }}
       >
         Fittracker
